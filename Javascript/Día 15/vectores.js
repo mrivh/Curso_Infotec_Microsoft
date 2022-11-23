41d1ada2b0d8a6adbfeb59b4b7014a03640386ac
mrivh <m.alicia.riv@gmail.com>
2022-11-22 22:45:05 -0600

    Update: JS - Día 15

---
+++ Javascript/Día 15/vectores.js
@@ -0,0 +1,12 @@
+/*
+
+1) Problema Ordenar un vector de enteros mediante el método sort, pasar una arrow function.
+
+2)Problema Definir un vector con un conjunto de elementos. Generar un nuevo vector llamando al método 
+filter pasando una función de flecha para definir cuáles rescatar. 
+
+3)Definir una función de flecha que reciba un valor entero y retorne otro valor entero aleatorio 
+comprendido entre 1 y el valor que llega como parámetro. Asignar dicha función de flecha a una constante 
+para permitir llamarla en sucesivas ocasiones.
+ 
+ */
